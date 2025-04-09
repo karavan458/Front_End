1556d9f0d46299e34870119c148fb69ea8177780
karavan458 <karavan458@gmail.com>
2025-04-09 11:11:13 +0900

    first commit

---
+++ JS/chapter01.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+console.log(1);
+console.log(true);
+console.log({name: '홍길동', age: 20, married: false});
+console.log('Hi!', 100, true, [1, 2, 3]);
+
+console.log(this);
+
+
+
