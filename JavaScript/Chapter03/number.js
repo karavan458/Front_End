b1c0c67db0609d85f561fb44996c890e229f9d29
karavan458 <karavan458@gmail.com>
2025-05-01 21:04:33 +0900

    add js Chatper3 - variable

---
+++ JavaScript/Chapter03/number.js
@@ -0,0 +1,18 @@
+let num1 = 10;  //정수
+let num2 = 3.14;    //실수
+
+console.log(typeof 2);
+
+//사칙연산
+console.log(1 + 2);
+console.log(3 - num1);
+console.log(num1 * num2);
+console.log(num1 /num2);
+
+//나머지를 구하는 연산
+console.log(10 % 3);
+
+//위의 연산들은 값을 반환
+const num3 = (num1 + num2) * 10;
+
+console.log(num3);
---
+++ JavaScript/Chapter03/undefined_null.js
@@ -0,0 +1,8 @@
+let x;
+console.log(x, typeof x);   //typeof가 반환하는 값은 문자열
+
+x = 1;
+console.log(x);
+
+x = null;
+console.log(x, typeof x);   //null 타입은 'object'로 변환 - 초기설계 부실
