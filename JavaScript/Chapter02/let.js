14b36146acdc5e7480a68879aeacd9cd659fdcf7
karavan458 <karavan458@gmail.com>
2025-05-01 20:46:17 +0900

    add js Chapter02

---
+++ JavaScript/Chapter02/let.js
@@ -0,0 +1,12 @@
+let a = 1;  //   a란 주머니에 1이라는 데이터 담기
+console.log(a);
+
+let b = a;  //  한 주머니 안에 든 데이터를 다른 주머니로
+console.log(b);
+
+a = 2;  //  a 주머니 안에 든 데이터 변경
+console.log(a);
+
+a = "안녕하세요";
+b = true;
+console.log(a, b);
---
+++ JavaScript/Chapter02/variable.js
@@ -0,0 +1,5 @@
+let darkModeOn = true;
+const PI = 3.1415926535;
+
+console.log(darkModeOn);
+console.log(PI);
