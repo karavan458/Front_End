cf455624bff76a63b285ce22affaaccffbf585f4
karavan458 <karavan458@gmail.com>
2025-05-01 20:37:41 +0900

    add js Chapter01

---
+++ JavaScript/Chapter01/comment.js
@@ -0,0 +1,20 @@
+console.log("Hello");
+
+// 사람이 읽기 위한 주석
+
+console.log("World");
+
+console.log(true);  // 코드의 오른쪽에도 작성할 수 있음
+// console.log(false);
+
+console.log(1);
+
+/* 
+여러 줄의 주석을 
+작성하는 방법입니다.
+*/
+
+//여러줄의 주석을 
+//작성하는 방법입니다.
+
+console.log(2);
---
+++ JavaScript/Chapter01/console.js
@@ -0,0 +1,5 @@
+// console.log('출력할 값');
+
+console.log(1, 2, 3);
+console.log([true, false], {a:1, b:2});
+console.log(console);
