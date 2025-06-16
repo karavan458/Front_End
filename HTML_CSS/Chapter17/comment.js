3772a06a197a8311e9423f86b7d048deba724ae0
karavan458 <karavan458@gmail.com>
2025-06-16 19:07:48 +0900

    add JS

---
+++ HTML_CSS/Chapter17/comment.js
@@ -0,0 +1,15 @@
+console.log("Hello");
+
+// 컴퓨터에게 알려주는 말이 아니야.
+
+console.log("World");
+
+console.log(true); // 코드의 오른쪽에도 작성할 수 있음
+// console.log(false);
+
+console.log(1);
+
+/*
+    여러줄의 주석을
+    작성하는 방법
+*/
