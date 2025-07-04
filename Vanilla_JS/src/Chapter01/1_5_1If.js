fa649fbcab9d28a20524c2aead5f4d637e398f4c
karavan458 <karavan458@gmail.com>
2025-07-05 01:24:38 +0900

    add VanillaJs conditional sentence

---
+++ Vanilla_JS/src/Chapter01/1_5_1If.js
@@ -0,0 +1,13 @@
+// 조건문
+// 특정 조건에 따라 코드를 실행하거나, 실행하지 않을 때 사용하는 구문
+
+// if 문
+let num = 50;
+
+if (num < 10) {
+    console.log("num은 10보다 작습니다.");
+} else if (num > 10) {
+    console.log("num은 10보다 큽니다.");
+} else {
+    console.log("num은 10과 같습니다.");
+}
---
+++ Vanilla_JS/src/Chapter01/1_5_2SwitchCase.js
@@ -0,0 +1,19 @@
+// switch case
+let fruit = "apple";
+
+switch (fruit) {
+    case "banana":
+        console.log("바나나");
+        break;
+    case "orange":
+        console.log("오렌지");
+        break;
+    case "apple":
+        console.log("사과");
+        break;
+    case "grape":
+        console.log("포도");
+        break;
+    default:
+        console.log("과일이 아니거나 등록되지 않은 과일입니다.");
+}
